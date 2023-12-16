b1d21f208400612ae395e60f11c43ab5f176ab33
Saksham Dwivedi <79015249+augsaksham@users.noreply.github.com>
2023-12-16 10:27:52 +0530

    Update local_infer.py

--- scripts/local_infer.py
+++ scripts/local_infer.py
@@ -79,8 +79,8 @@ def run(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--reference_model_name', type=str, default="gpt-j-6B")
-    parser.add_argument('--scoring_model_name', type=str, default="gpt-neo-2.7B")
+    parser.add_argument('--reference_model_name', type=str, default="bloom-30b")
+    parser.add_argument('--scoring_model_name', type=str, default="gpt-2.7_qunat")
     parser.add_argument('--dataset', type=str, default="xsum")
     parser.add_argument('--ref_path', type=str, default="./local_infer_ref")
     parser.add_argument('--device', type=str, default="cuda")
